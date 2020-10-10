8f1683cf30cd32864a0d17b37901225de70643c1
yuto urushima <yuto259233@gmail.com>
2020-10-10 22:02:26 +0900

    js gulp圧縮 try

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,34 @@
-// gulpプラグインの読み込み
+'use strict';
+
+// gulp設定部分
 const gulp = require("gulp");
-
-// Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
+// const rename = require("gulp-rename");
+// const uglify = require("gulp-uglify");
 
-// styleMedia.scssのタスクを作成する
 gulp.task("default", function(){
-    // style.scssファイルを監視
-    return gulp.watch("styles/style.scss", function(){
-        // styleMedia.scssの更新があった場合の処理
-
-        // style.scssファイルを取得
+    return gulp.watch("src/styles/style.scss", function(){
         return (
             gulp
-                .src("styles/style.scss")
-                // Sassのコンパイルを実行
+                .src("src/styles/style.scss")
                 .pipe(
                     sass({
                     outputStyle: "expanded"
                     })
-                    // Sassのコンパイルエラーを表示
                     .on("error", sass.logError)
                 )
-                // cssフォルダーを以下に保存
                 .pipe(gulp.dest("dist/styles"))
-                
-        );
-    });
-});
+        )
+    })
+})
+
+
+// gulp.task("js-minify", function() {
+//     gulp.src('src/scripts/*.js')
+//         .pipe(uglify())
+//         .pipe(rename({
+//             extname: '.min.js'
+//         }))
+//         .pipe(gulp.dest("dist/scripts/"));
+// });
 
