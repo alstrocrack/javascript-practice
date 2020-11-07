cc1d29480f0661ae4e61f6e50737e2ec86284ba3
yuto urushima <yuto259233@gmail.com>
2020-11-07 18:18:54 +0900

    KV周りのjs 記述

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', () => {
     headerShow();
+    kvShow();
+    KVinit();
   });
 
 //   ヘッダーのフェードイン
@@ -19,7 +21,7 @@ headerMenu.forEach(e => {
     e.addEventListener('mouseover', () => {
         gsap.to(e, {
             color: '#87cefa',
-            duration: 0.5,
+            duration: 0.3,
             ease: Power4.easeIn,
             onStart: () => {
                 e.innerHTML = "click";
@@ -29,7 +31,7 @@ headerMenu.forEach(e => {
     e.addEventListener('mouseleave', () => {
         gsap.to(e, {
             color: '#df5656',
-            duration: 0.8,
+            duration: 0.3,
             ease: Power4.easeIn,
             onStart: () => {
                 e.innerHTML = "reverse";
@@ -39,5 +41,37 @@ headerMenu.forEach(e => {
     console.log(e);
 });
 
+
+// KVのフェードイン
+const kvText = document.querySelector(".kv-text_content");
+gsap.set(kvText, {
+    y: 40,
+    opacity: 0,
+})
+function kvShow(){
+    gsap.to(kvText, 3, {
+        y: 0,
+        delay: 2,
+        ease: Power2.easeOut,
+        opacity: 1,
+    });
+}
+
+// KVのスライド
+function KVinit(){
+    const kvSwiper = new Swiper('.kv-swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 1000,
+        centeredSlides: true,
+        effect: 'coverflow',
+        autoplay:{
+            delay: 7000,
+            stopOnlastSlide: false,
+            disableOnInteraction: false,
+        },
+    });
+}
 
 
